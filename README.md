41233f234a2345382b84689c1d9eb8b256913ca8
Hanming Zhu <hanming@huawen.com>
2022-03-09 17:59:39 +0800

    Format files

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# habitat-heroes
+# Habitat Heroes
+
 ## Requirements
 
 [Node.js](https://nodejs.org) is required to install dependencies and run scripts via `npm`.
 
 ## Available Commands
 
-| Command | Description |
-|---------|-------------|
-| `npm install` | Install project dependencies |
-| `npm start` | Build project and open web server running project |
+| Command         | Description                                                                     |
+| --------------- | ------------------------------------------------------------------------------- |
+| `npm install`   | Install project dependencies                                                    |
+| `npm start`     | Build project and open web server running project                               |
 | `npm run build` | Builds code bundle with production settings (minification, uglification, etc..) |
 
 ## Writing Code
--- src/objects/Skeleton.js
+++ src/objects/Skeleton.js
@@ -1,43 +1,43 @@
 // GameObject Skeleton
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
 const directions = {
-  west: {offset: 0, x: -2, y: 0, opposite: 'east'},
-  northWest: {offset: 32, x: -2, y: -1, opposite: 'southEast'},
-  north: {offset: 64, x: 0, y: -2, opposite: 'south'},
-  northEast: {offset: 96, x: 2, y: -1, opposite: 'southWest'},
-  east: {offset: 128, x: 2, y: 0, opposite: 'west'},
-  southEast: {offset: 160, x: 2, y: 1, opposite: 'northWest'},
-  south: {offset: 192, x: 0, y: 2, opposite: 'north'},
-  southWest: {offset: 224, x: -2, y: 1, opposite: 'northEast'}
+  west: { offset: 0, x: -2, y: 0, opposite: 'east' },
+  northWest: { offset: 32, x: -2, y: -1, opposite: 'southEast' },
+  north: { offset: 64, x: 0, y: -2, opposite: 'south' },
+  northEast: { offset: 96, x: 2, y: -1, opposite: 'southWest' },
+  east: { offset: 128, x: 2, y: 0, opposite: 'west' },
+  southEast: { offset: 160, x: 2, y: 1, opposite: 'northWest' },
+  south: { offset: 192, x: 0, y: 2, opposite: 'north' },
+  southWest: { offset: 224, x: -2, y: 1, opposite: 'northEast' },
 };
 
 const anims = {
   idle: {
     startFrame: 0,
     endFrame: 4,
-    speed: 0.2
+    speed: 0.2,
   },
   walk: {
     startFrame: 4,
     endFrame: 12,
-    speed: 0.15
+    speed: 0.15,
   },
   attack: {
     startFrame: 12,
     endFrame: 20,
-    speed: 0.11
+    speed: 0.11,
   },
   die: {
     startFrame: 20,
     endFrame: 28,
-    speed: 0.2
+    speed: 0.2,
   },
   shoot: {
     startFrame: 28,
     endFrame: 32,
-    speed: 0.1
-  }
+    speed: 0.1,
+  },
 };
 
 export class Skeleton extends Phaser.GameObjects.Image {
@@ -59,16 +59,13 @@ export class Skeleton extends Phaser.GameObjects.Image {
     scene.time.delayedCall(this.anim.speed * 1000, this.changeFrame, [], this);
   }
 
-  changeFrame ()
-  {
+  changeFrame() {
     this.f++;
 
     let delay = this.anim.speed;
 
-    if (this.f === this.anim.endFrame)
-    {
-      switch (this.motion)
-      {
+    if (this.f === this.anim.endFrame) {
+      switch (this.motion) {
         case 'walk':
           this.f = this.anim.startFrame;
           this.frame = this.texture.get(this.direction.offset + this.f);
@@ -77,52 +74,72 @@ export class Skeleton extends Phaser.GameObjects.Image {
 
         case 'attack':
           delay = Math.random() * 2;
-          this.scene.time.delayedCall(delay * 1000, this.resetAnimation, [], this);
+          this.scene.time.delayedCall(
+            delay * 1000,
+            this.resetAnimation,
+            [],
+            this
+          );
           break;
 
         case 'idle':
           delay = 0.5 + Math.random();
-          this.scene.time.delayedCall(delay * 1000, this.resetAnimation, [], this);
+          this.scene.time.delayedCall(
+            delay * 1000,
+            this.resetAnimation,
+            [],
+            this
+          );
           break;
 
         case 'die':
           delay = 6 + Math.random() * 6;
-          this.scene.time.delayedCall(delay * 1000, this.resetAnimation, [], this);
+          this.scene.time.delayedCall(
+            delay * 1000,
+            this.resetAnimation,
+            [],
+            this
+          );
           break;
       }
-    }
-    else
-    {
+    } else {
       this.frame = this.texture.get(this.direction.offset + this.f);
 
       this.scene.time.delayedCall(delay * 1000, this.changeFrame, [], this);
     }
   }
 
-  resetAnimation ()
-  {
+  resetAnimation() {
     this.f = this.anim.startFrame;
 
     this.frame = this.texture.get(this.direction.offset + this.f);
 
-    this.scene.time.delayedCall(this.anim.speed * 1000, this.changeFrame, [], this);
+    this.scene.time.delayedCall(
+      this.anim.speed * 1000,
+      this.changeFrame,
+      [],
+      this
+    );
   }
 
-  update ()
-  {
-    if (this.motion === 'walk')
-    {
+  update() {
+    if (this.motion === 'walk') {
       this.x += this.direction.x * this.speed;
 
-      if (this.direction.y !== 0)
-      {
+      if (this.direction.y !== 0) {
         this.y += this.direction.y * this.speed;
         this.depth = this.y + 64;
       }
 
       //  Walked far enough?
-      if (Phaser.Math.Distance.Between(this.startX, this.startY, this.x, this.y) >= this.distance)
-      {
+      if (
+        Phaser.Math.Distance.Between(
+          this.startX,
+          this.startY,
+          this.x,
+          this.y
+        ) >= this.distance
+      ) {
         this.direction = directions[this.direction.opposite];
         this.f = this.anim.startFrame;
         this.frame = this.texture.get(this.direction.offset + this.f);
