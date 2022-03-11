7f637d8f12030456436599f2f143466370c40f2d
seeziyang <44959961+seeziyang@users.noreply.github.com>
2022-03-11 22:40:06 +0800

    Add prev and next buttons

--- src/scenes/ShopScene.js
+++ src/scenes/ShopScene.js
@@ -1,6 +1,8 @@
 import Phaser from 'phaser';
 
 import closebutton from '../assets/game_menu/close_button.png';
+import nextbutton from '../assets/game_menu/next_button.png';
+import prevbutton from '../assets/game_menu/prev_button.png';
 import background from '../assets/game_menu/woodboard_no_cblogo.png';
 import Button from '../objects/Button';
 
@@ -9,6 +11,10 @@ export class ShopScene extends Phaser.Scene {
   screenCenterX;
 
   screenCenterY;
+
+  prevButton;
+
+  nextButton;
 
   constructor() {
     super({
@@ -19,24 +25,53 @@ export class ShopScene extends Phaser.Scene {
   preload() {
     this.load.image('background', background);
     this.load.image('closebutton', closebutton);
+    this.load.image('prevbutton', prevbutton);
+    this.load.image('nextbutton', nextbutton);
 
     this.screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
     this.screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
   }
 
   create() {
-    this.add.image(this.screenCenterX, this.screenCenterY + 10, 'background').setScale(0.75);
+    this.add.image(this.screenCenterX, this.screenCenterY + 10, 'background').setScale(0.85);
 
+    this.add.text(640, 100, 'Shop', {
+      fontFamily: 'Graduate',
+      fontSize: 90,
+      color: '#fff',
+      strokeThickness: 2
+    });
 
-    const closeButton = new Button(this, this.screenCenterX + 340, this.screenCenterY - 218, 'closebutton')
+    const closeButton = new Button(this, this.screenCenterX + 390, this.screenCenterY - 240, 'closebutton')
       .setDownTexture('closebutton');
-    closeButton.scale = 0.8;
+    closeButton.depth = 800;
+    closeButton.scale = 0.6;
     closeButton.setButtonName('Close');
     closeButton.on('pointerup', () => {
       this.scene.stop('ShopScene');
       this.scene.resume('HabitatHeroesScene');
     });
-
     this.add.existing(closeButton);
+
+    this.prevButton = new Button(this, this.screenCenterX - 360, this.screenCenterY + 50, 'prevbutton')
+      .setDownTexture('prevbutton');
+    this.prevButton.scale = 0.4;
+    this.prevButton.depth = 800;
+    this.prevButton.setButtonName('Previous Page');
+    this.prevButton.on('pointerup', () => {
+      // TODO
+    });
+    this.add.existing(this.prevButton);
+
+    this.nextButton = new Button(this, this.screenCenterX + 368, this.screenCenterY + 50, 'nextbutton')
+      .setDownTexture('nextbutton');
+    this.nextButton.scale = 0.4;
+    this.nextButton.depth = 800;
+    this.nextButton.setButtonName('Next Page');
+    this.nextButton.on('pointerup', () => {
+      // TODO
+    });
+    this.add.existing(this.nextButton);
+
   }
 }
