93ed024d150ffe877973434085e59116aed2b2cd
Jing Ying <e0323887@u.nus.edu>
2022-03-13 18:43:54 +0800

    Enable storing of data of items on map

---
+++ src/reducers/mapItemsReducer.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+};
+
+const mapItemsSlice = createSlice({
+  name: 'mapItems',
+  initialState,
+  reducers: {
+    addToMap: (state, action) => {
+      const coordX = action.payload.coordinates[0];
+      const coordY = action.payload.coordinates[1];
+      state[[coordX, coordY]] = {
+        coordinates: action.payload.coordinates,
+        depth: action.payload.depth,
+        spritesheet: action.payload.spritesheet,
+        frame: action.payload.frame
+      };
+    },
+  },
+});
+
+export const { addToMap } = mapItemsSlice.actions;
+
+export default mapItemsSlice.reducer;
